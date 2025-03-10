758960aaa27cf654d9ca7dc1577d192a0eaa1542
Mati Joseph <crucialniccur@gmail.com>
2025-03-10 17:24:01 +0300

    define the superbowlWin function

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+let obj = { year, results };
+function superbowlWin(record) {
+  let win = record.find((record) => record.result === "W");
+  if (win) {
+    return win.year;
+  }
+}
